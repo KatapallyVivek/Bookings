03bb0139377e8599c126c7fa038464e33c52f3a0
Vivek Katapally <vivekkatapally03@gmail.com>
2026-03-28 19:50:38 +0530

    Add useEffect and Prices

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,32 @@ import { useState } from 'react'
 import './App.css'
 import Tickets from './booking'
 function App() {
+
+  // details
   let trainHeading = "Train"
   let trainImg = 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiiBX_ZApT9shr4Q_San4URirNfA6CdzqkCBMgZigusB-5G7mbr6M24hcYvwrtZRQTI0kzzoaaWcS1tL5Pe8wXylXAQEy6yOi2oSerQJ14IuzAjvg8orFRA99bxlS-UdYbZua7WgzRRH0Uh1umazoNpFsdG6Fs0qOXiehJRG4mAY2LNxu5n_5L6W0Kj7Rw/s1200/Vande%20Bharat%20Red%20Gray.jpg'
+  let trainAmt = 500;
+
   let flightHeading = "Flight"
   let flightImg = 'https://images.stockcake.com/public/f/b/6/fb6acf1b-fcaa-4152-b48b-6b0e2b0c5374_large/airplane-mid-flight-stockcake.jpg'
+  let flightAmt = 5000;
+  
   let busHeading = "Bus"
   let busImg = 'https://www.shutterstock.com/image-illustration/image-bus-on-road-600nw-2568680639.jpg'
+  let busAmt = 100;
+
   let shipHeading = "Ship"
   let shipImg = 'https://images.pexels.com/photos/813011/pexels-photo-813011.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+  let shipAmt = 7000;
 
+  
   return (
     <>
       <div className='container'>
-        <Tickets Heading={trainHeading} imgUrl={trainImg}></Tickets>
-        <Tickets Heading={flightHeading} imgUrl={flightImg}></Tickets>
-        <Tickets Heading={busHeading} imgUrl={busImg} ></Tickets>
-        <Tickets Heading={shipHeading} imgUrl={shipImg}></Tickets>
+        <Tickets Heading={trainHeading} imgUrl={trainImg} amount={trainAmt}></Tickets>
+        <Tickets Heading={flightHeading} imgUrl={flightImg} amount={flightAmt}></Tickets>
+        <Tickets Heading={busHeading} imgUrl={busImg} amount={busAmt}></Tickets>
+        <Tickets Heading={shipHeading} imgUrl={shipImg} amount={shipAmt}></Tickets>
       </div>
     </>
   )
--- frontend/src/booking.jsx
+++ frontend/src/booking.jsx
@@ -1,27 +1,41 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './booking.css'
-function Tickets({Heading , imgUrl}) {
+
+function Tickets({Heading , imgUrl , amount}) {
     const [state, setState] = useState(0);
+    const [availble, setAvailble] = useState(100)
+
+    let totalAmt = state * amount;
+
+    useEffect(function () {
+        if(state < 0 ){
+            setState(0);
+            setAvailble(100);
+        }
+    });
 
     function bookTicket() {
         setState(state + 1);
+        setAvailble(availble -1);
     }
     function cancelTicket() {
-        if(state > 0) {
 
-            setState(state - 1)
-        }
+            setState(state - 1);
+            setAvailble(availble + 1);
     }
   return (
     <div>
         
         <div className='Main'>
             <h1>{Heading}</h1>
+            <h3>Amount per one ticket is {amount}</h3>
+            <h2>{availble} tickets are available</h2>
             <h1>{state} tickets booked</h1>
             <img src={imgUrl} alt="" />
             <br />
             <button onClick={bookTicket}>Book</button>
             <button onClick={cancelTicket}>Cancel</button>
+            <p>The total cost is {totalAmt} </p>
         </div>
 
     </div>
